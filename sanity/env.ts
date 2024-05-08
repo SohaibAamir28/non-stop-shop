ca7d91d3920c2fd78171f9ac66d5b3d811a0b5d0
Sohaib Aamir <103496288+SohaibAamir28@users.noreply.github.com>
2024-05-08 09:05:28 +0500

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-05-08'
 
 export const dataset = assertValue(
-  "production",
+  NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  "kb27zuzn",
+  "NEXT_PUBLIC_SANITY_PROJECT_ID",
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
